749da00245a69aade1f14660daad6b476046a492
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-19 19:10:45 +0200

    [UPD] fine-esercizio

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 //recupero gli elementi del dom che mi servono
 const mailList = document.getElementById(`mail-list`);
 
-// const endpoint = `httpsflynn.boolean.careersexercisesapirandommail`;
-
 //chiamata AJAX
 const emailGenerator = () => {
-  axios.get(endpoint).then((mail) => {
-    //ciclo per 10 per generare 10 mail
-    for ()
-      //recupero il risultato della chiamata ajax
-  });
+  //ciclo per 10 per generare 10 mail
+  for (let i = 0; i < 10; i++) {
+    //recupero il risultato della chiamata ajax
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((resp) => {
+        const email = resp.data.response;
+        mailList.innerHTML += `<li>${email}</li>`;
+      });
+  }
 };
+
+//richiamo la funzione
+emailGenerator();
