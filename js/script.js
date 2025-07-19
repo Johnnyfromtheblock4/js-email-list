e9d0ac53e6006064e15b19e330598476800acc41
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-19 19:02:51 +0200

    [UPD] nuovo-html

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 //recupero gli elementi del dom che mi servono
-const mail1 = document.getElementById(`mail1`);
-const mail2 = document.getElementById(`mail2`);
-const mail3 = document.getElementById(`mail3`);
-const mail4 = document.getElementById(`mail4`);
-const mail5 = document.getElementById(`mail5`);
-const mail6 = document.getElementById(`mail6`);
-const mail7 = document.getElementById(`mail7`);
-const mail8 = document.getElementById(`mail8`);
-const mail9 = document.getElementById(`mail9`);
-const mail10 = document.getElementById(`mail10`);
+const mailList = document.getElementById(`mail-list`);
 
-const endpoint = `httpsflynn.boolean.careersexercisesapirandommail`;
+// const endpoint = `httpsflynn.boolean.careersexercisesapirandommail`;
 
+//chiamata AJAX
+const emailGenerator = () => {
+  axios.get(endpoint).then((mail) => {
+    //ciclo per 10 per generare 10 mail
+    for ()
+      //recupero il risultato della chiamata ajax
+  });
+};
