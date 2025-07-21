687eefc469128ab6e2df7c81696ca3433423b249
Johnnyfromtheblock4 <simo.visco4@gmail.com>
2025-07-21 10:03:09 +0200

    [UPD] correzione-es-base

--- js/script.js
+++ js/script.js
@@ -1,19 +1,28 @@
 //recupero gli elementi del dom che mi servono
 const mailList = document.getElementById(`mail-list`);
 
-//chiamata AJAX
-const emailGenerator = () => {
-  //ciclo per 10 per generare 10 mail
-  for (let i = 0; i < 10; i++) {
-    //recupero il risultato della chiamata ajax
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((resp) => {
-        const email = resp.data.response;
-        mailList.innerHTML += `<li>${email}</li>`;
-      });
-  }
-};
+//definisco un array in cui inserire le mail
+const mails = [];
 
-//richiamo la funzione
-emailGenerator();
+//chiamata AJAX
+//ciclo per 10 per generare 10 mail
+for (let i = 0; i < 10; i++) {
+  //recupero il risultato della chiamata ajax
+  axios
+    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then((resp) => {
+      //recupero la mail in risposta alla chiamata
+      const email = resp.data.response;
+
+      //inserisco la mail recuperata all'interno dell'array
+      mails.push(email);
+
+      //inserisco tramite HTML degli li all'interno della ul
+      mailList.innerHTML += `<li>${email}</li>`;
+
+      //quando l'array ha 10 elementi, allora tolgo la classe d-none dall'ul
+      if (mails.length === 10) {
+        mailList.classList.remove("d-none");
+      }
+    });
+}
